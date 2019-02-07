c3bd218141386605246b771b77131d37164c977b
Andrew Violan <andrew.violan@gmail.com>
2019-02-06 19:00:42 -0800

    modified handlebars

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var request = require('request');
 var cheerio = require('cheerio');
 
+var port = process.env.PORT || 3000;
+
 var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
@@ -18,13 +20,16 @@ app.use(express.static(process.cwd() + '/public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 if(process.env.MONGODB_URI){
   mongoose.connect('mongodb://heroku_csxw199c:ov188d6b52pkqecidudadj89hh@ds223685.mlab.com:23685/heroku_csxw199c');
 }
 else{
   mongoose.connect('mongodb://localhost/mongoHeadlines');
 }
-var db = mongoose.connect;
+
+mongoose.connect(MONGODB_URI);
 
 var Comment = require('./models/note.js');
 var Article = require('./models/articles.js');
@@ -32,7 +37,6 @@ var Article = require('./models/articles.js');
 var router = require('./controllers/controller.js');
 app.use('/', router);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-  console.log('Running on port: ' + port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
